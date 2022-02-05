71b1bea0e434344cbff064005935c74ac7a84724
Zsoleesz <98643747+Zsoleesz@users.noreply.github.com>
2022-02-05 12:23:20 +0100

    js módosítások

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ loadSong(songs[songIndex]);
 //Frissítjük a zenék adatait
 function loadSong(song){
 
+    
+
     title.innerText = song;
     audio.src = `music/${song}.mp3`;
     cover.src = `images/${song}.jpg`;
